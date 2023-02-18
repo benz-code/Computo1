645d0efcffac647f9856f278acfc0ba04dba7f44
benz-code <87821819+benz-code@users.noreply.github.com>
2023-02-18 14:11:06 -0600

    Semana 2
    Esta carpeta corresponde a la semana 2 de la asignatura Programación Computacional IV

---
+++ Semana 2/main.js
@@ -0,0 +1,33 @@
+//Constantes del botón ocultar y el div
+const hidebtn= document.querySelector('#hideElement')
+const listdiv = document.querySelector('#list')
+
+
+
+//Agregar evento al botón hidebtn
+hidebtn.addEventListener('click', ()=>{
+    if(listdiv.style.display== 'none'){
+        listdiv.style.display = 'block'
+        hidebtn.textContent = 'Ocultar'
+    } else {
+        listdiv.style.display = 'none'
+        hidebtn.textContent = 'Mostrar'
+    }
+    
+
+
+})
+
+//Agregar elementos a la lista
+const addItemInput = document.querySelector('#addItem')
+const addItemButton = document.querySelector('button#addItemButton')
+
+addItemButton.addEventListener('click', () => {
+    let list = document.querySelector('ul')
+    let li = document.createElement('li')
+    li.textContent = addItemInput.value
+    list.appendChild(li)
+    
+})
+
+
